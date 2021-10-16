9399d1615bd2cb0c342970f0c7e4653ec30c49f1
Nelson <eunelsonmauricio@gmail.com>
2021-10-16 09:50:41 -0300

    Criação do script2.js com estrutura switch

---
+++ Aula03/js/script2.js
@@ -0,0 +1,45 @@
+var cor = prompt("Digite uma cor, que pode ser \n Vermelho, Verde, Azul, Amarelo, Preto, Cinza");
+
+var fundo = "";
+
+/*
+Para transformar os caracteres em maiúsculo ou minúsculo podemos usar os seguintes
+comandos:
+toUpperCase() -> para letras maiúsculas
+toLowerCase() -> para  letras minúsculas
+*/
+cor = cor.toLowerCase();
+
+
+switch(cor){
+    case "vermelho":
+        fundo="red";
+        break;
+    
+    case "verde":
+        fundo="green";
+        break;
+
+    case "azul":
+        fundo="blue";
+        break;   
+
+    case "amarelo":
+        fundo="yellow";
+         break;   
+
+case "preto":
+        fundo="black";
+        break; 
+
+
+case "cinza":
+        fundo="gray";
+        break; 
+
+default:
+    fundo="white";
+    break;
+    }   
+document.body.style.backgroundColor=fundo; 
+
